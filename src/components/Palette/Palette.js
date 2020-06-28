1d1a9cba1ce71af5ae889592b26a0d5113c73ff4
Yasin <yasinvural0@gmail.com>
2020-06-28 20:23:48 +0300

    copyAnimation font color is calculated depend on backgroundColor

--- src/components/Palette/Palette.js
+++ src/components/Palette/Palette.js
@@ -25,6 +25,12 @@ const Palette = ({ palette }) => {
 
   const copyClipboard = (e, code) => {
     const copied = e.currentTarget.children[1];
+    const isColorDark = isColorTooDark(code);
+    if (isColorDark) {
+      copied.classList.add("whiteFontColor");
+    } else {
+      copied.classList.add("blackFontColor");
+    }
 
     const addAnimation = (copied) => {
       copied.classList.add("copyAnimation");
@@ -43,6 +49,22 @@ const Palette = ({ palette }) => {
     document.execCommand("copy");
     textField.remove();
   };
+
+  const isColorTooDark = (code) => {
+    const color = code.substring(1); // strip #
+    const rgb = parseInt(color, 16); // convert rrggbb to decimal
+    const red = (rgb >> 16) & 0xff; // extract red
+    const green = (rgb >> 8) & 0xff; // extract green
+    const blue = (rgb >> 0) & 0xff; // extract blue
+    const luma = 0.2126 * red + 0.7152 * green + 0.0722 * blue; // per ITU-R BT.709
+
+    if (luma > 70) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
   return (
     <div className="palette-container">
       {renderPalette()}
